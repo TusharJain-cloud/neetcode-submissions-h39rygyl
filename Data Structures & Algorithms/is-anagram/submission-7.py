2aa14d2f0374bbb5655896a607a74c5033a0c335
Tushar Jain <66814919+TusharJain-cloud@users.noreply.github.com>
2026-04-13 11:05:41 -0400

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # Brute Force but the T.C -> O(n^2)
+        if len(s) != len(t):
+            return False
+        for i in s:
+            if i in t:
+                t = t.replace(i, "", 1)
+
+        return t == ""
+        # Now using Hashmaps
+        # if len(s) != len(t):
+        #     return False
+
+        # countS, countT = {}, {}
+
+        # for i in countS:
+        #     countS[i] = 1 + countS.get(countS[i], 0)
+        #     countT[i] = 1 + countT.get(countT[i], 0)
+        
+        # for c in countS:
+        #     if countS[c] != countT.get(countT[c],0):
+        #         return False
+        
+        # return True
+        # return Counter(s) == Counter(t)
+        # sorting with space of O(1) and time of O(n log n)
+        return sorted(s) == sorted(t)
+        
