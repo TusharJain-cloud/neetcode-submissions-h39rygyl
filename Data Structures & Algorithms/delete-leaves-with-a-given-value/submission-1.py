88e93fc16439b2b8c8d8bc52925f787ccd300469
Tushar Jain <66814919+TusharJain-cloud@users.noreply.github.com>
2026-09-11 14:43:24 +0530

    Add: delete-leaves-with-a-given-value - submission-1

---
+++ Data Structures & Algorithms/delete-leaves-with-a-given-value/submission-1.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        # dfs 
+        # if not root:
+        #     return
+        
+        # root.left = self.removeLeafNodes(root.left, target)
+        # root.right = self.removeLeafNodes(root.right, target)
+
+        # if not root.left and not root.right and target == root.val:
+        #     return None
+        
+        # return root
+
+        # Iterative
+
+        stack = [root]
+        visit = set()
+        parent = {root : None}
+
+        while stack:
+            node = stack.pop()
+            if not node.left and not node.right:
+                if node.val == target:
+                    p = parent[node]
+                    if not p:
+                        return None
+                    if p.left == node:
+                        p.left = None
+                    if p.right == node:
+                        p.right = None
+            elif node not in visit:
+                visit.add(node)
+                stack.append(node)
+                if node.left:
+                    stack.append(node.left)
+                    parent[node.left] = node
+                if node.right:
+                    stack.append(node.right)
+                    parent[node.right] = node
+        return root
+
